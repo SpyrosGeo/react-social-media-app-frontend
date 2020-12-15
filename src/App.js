970ab23a3a6c7b7f69d1083b2d5bac735911ad12
SpyrosGeo <xarodasimba@gmail.com>
2020-12-15 13:43:51 +0200

    added basic routes and implemented semantic ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
 import ApolloProvider from './ApolloProvider'
+import {BrowserRouter as Router,Route } from 'react-router-dom'
+
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react'
+import './App.css';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MenuBar from './components/MenuBar';
 
 function App() {
   return (
     <ApolloProvider>
-      <div>
-        <h1>Hello there dudes</h1>
-      </div>
+      <Router>
+        <Container>
+        <MenuBar/>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        </Container>
+      </Router>
     </ApolloProvider>
   );
 }
